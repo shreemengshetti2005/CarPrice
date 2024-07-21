26c85fea4835eb980cdec300861e4797365f8e7d
LaminarFlow24 <148671685+LaminarFlow24@users.noreply.github.com>
2024-07-21 10:12:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
 import numpy as np
-
-
 import matplotlib.pyplot as plt
 from tensorflow.keras import datasets, models, layers
 import pickle
@@ -18,76 +15,88 @@ st.title("Know the correct price of your Car!")
 df = pd.read_csv('cardekho_dataset.csv')
 
 encoder = OneHotEncoder()
-df_encoded = encoder.fit_transform(df[['brand','model','seller_type','fuel_type','transmission_type']])
+df_encoded = encoder.fit_transform(df[['brand','model', 'seller_type', 'fuel_type', 'transmission_type']])
+odf = pd.DataFrame.sparse.from_spmatrix(df_encoded)
+dff = df[['vehicle_age', 'km_driven', 'mileage', 'engine', 'max_power', 'selling_price']]
+cdf = pd.concat([odf, dff], axis=1)
+cdf.columns = cdf.columns.astype(str)
 
-dff = df[['vehicle_age','km_driven','mileage','engine','max_power','selling_price']]
-scaler = MinMaxScaler(feature_range=(0, 1))
-scaler.fit(dff)
+scaler = StandardScaler()
+scaler.fit(cdf)
 
-scaler_y = MinMaxScaler(feature_range=(0, 1))
+# Initialize StandardScaler
+scaler_y = StandardScaler()
+
 # Fit and transform the target variable
 scaler_y.fit(df[['selling_price']])
 
-
-
 # Load the model from file
-with open('xgboost_model.pkl', 'rb') as f:
+with open('xgboost_model5.pkl', 'rb') as f:
     loaded_model = pickle.load(f)
 
-# Use the loaded model for predictions or further analysis
-# brands = pd.Series(df['brand'].unique())
-brand = st.selectbox("Enter brand",(df['brand'].unique()))
+# Input fields for the user
+brand = st.selectbox("Enter brand", (df['brand'].unique()))
+bbbb = df[df['brand'] == brand]
+model = st.selectbox("Enter Model", bbbb['model'].unique())
+# car_name = st.selectbox("Enter name",df['car_name'].unique())
 
-model = st.selectbox("Enter Model",(df['model'].unique()))
-
-vehicle_age = st.number_input("Enter age",value=9)
-
-km_driven = st.number_input("Enter km driven",value=120000)
-
-seller_type = st.selectbox("Enter seller type",df['seller_type'].unique())
-
-fuel_type = st.selectbox("Enter fuel type",df['fuel_type'].unique())
-
-transmission_type = st.selectbox("Enter transmission type",df['transmission_type'].unique())
-
-mileage = st.number_input("Whats is its mileage (Kmpl)?",value=19.7)
-
-engine = st.number_input("Engine capacity in CC (Cubic Centimeters)",value=796)
-
-max_power = st.number_input("Max power in BHP",value=46.3)
-
-seats = st.number_input("number of seats",1,9,value=5)
-selling_price = 0
-############################################################
+aaaa = df[df['model'] == model]
 
 
-X = [[brand,model,vehicle_age,km_driven,seller_type,fuel_type,transmission_type,mileage,engine,max_power,seats,selling_price]]
+aaaa['engine'] = [int(value) for value in aaaa['engine']]
+aaaa['max_power'] = [int(value) for value in aaaa['max_power']]
+# aaaa['engine'] = [str(value) for value in aaaa['engine']]
+# aaaa['max_power'] = [str(value) for value in aaaa['max_power']]
 
-columns = ['brand', 'model', 'vehicle_age', 'km_driven', 'seller_type',
-           'fuel_type', 'transmission_type', 'mileage', 'engine', 'max_power', 'seats','selling_price']
 
-X = pd.DataFrame(X,columns=columns)
+vehicle_age = st.number_input("Enter age", value=9)
+km_driven = st.number_input("Enter km driven", value=120000)
+# seller_type = st.selectbox("Enter seller type", df['seller_type'].unique())
+seller_type = 'Individual'
+fuel_type = st.selectbox("Enter fuel type", df['fuel_type'].unique())
+transmission_type = st.selectbox("Enter transmission type", df['transmission_type'].unique())
+mileage = st.number_input("Whats is its mileage (Kmpl)?", value=19.7)
+# engine = st.number_input("Engine capacity in CC (Cubic Centimeters)", value=796)
+engine = st.selectbox("Enter Engine capacity (cc)", (aaaa['engine'].unique()))
+# engine = int(engine)
+max_power = st.selectbox("Max power in BHP",(aaaa['max_power'].unique()))
+# max_power = int(max_power)
+seats = st.selectbox("Number of seats",aaaa['seats'].unique())
+# seats = int(seats)
 
-X_enc = encoder.transform(X[['brand','model','seller_type','fuel_type','transmission_type']])
+# Prepare the input data
+X = [[brand, model , vehicle_age, km_driven, seller_type, fuel_type, transmission_type, mileage, engine, max_power, seats, 0]]
+columns = ['brand','model', 'vehicle_age', 'km_driven', 'seller_type', 'fuel_type', 'transmission_type', 'mileage', 'engine', 'max_power', 'seats', 'selling_price']
+X = pd.DataFrame(X, columns=columns)
 
-Xedf = pd.DataFrame.sparse.from_spmatrix(X_enc)
+# One-hot encode the categorical features
+X_enc = encoder.transform(X[['brand','model', 'seller_type', 'fuel_type', 'transmission_type']])
+X_enc = pd.DataFrame.sparse.from_spmatrix(X_enc)
 
-Xndf = X[['vehicle_age','km_driven','mileage','engine','max_power','selling_price']]
+# Combine encoded and numerical features
+X_ndf = X[['vehicle_age', 'km_driven', 'mileage', 'engine', 'max_power', 'selling_price']]
+X_ccdf = pd.concat([X_enc, X_ndf], axis=1)
 
-X_norm = scaler.transform(Xndf)
+# Ensure the column names are string
+X_ccdf.columns = X_ccdf.columns.astype(str)
 
-X_norm = pd.DataFrame(X_norm, columns=Xndf.columns)
+# Standardize the features
+X_norm = scaler.transform(X_ccdf)
+X_norm = pd.DataFrame(X_norm, columns=X_ccdf.columns)
 X_norm = X_norm.drop(columns=['selling_price'])
 
-Xcdf = pd.concat([Xedf,X_norm],axis = 1)
-
-X_for_tree = xgb.DMatrix(Xcdf)
+# Prepare data for prediction
+X_for_tree = xgb.DMatrix(X_norm)
 y = loaded_model.predict(X_for_tree)
 
+# Inverse transform the predicted value
 y = pd.DataFrame(y)
 y.columns = ['selling_price']
 yd = scaler_y.inverse_transform(y)
 yd = pd.DataFrame(yd)
 yd.columns = ['selling_price']
 
-st.write(yd)
+st.header(f"Predicted Selling Price: {int(yd['selling_price'][0])}")
+
+st.text_input("Have significant damages to your car? Tell us about it here and our support staff will manually help you find more accurate price for your car")
+
