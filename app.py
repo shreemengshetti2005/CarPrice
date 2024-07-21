077a9824f6fbf1a59b19a564cdf736367a260a40
Soham7997 <161009997+Soham7997@users.noreply.github.com>
2024-07-21 23:57:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,8 +55,8 @@ st.markdown(
       position: absolute;
       top: 0;
       left: 0;
-      width: 500%;
-      height: 500%;
+      width: 900%;
+      height: 900%;
       background: rgba(0, 0, 0, 0.71); /* Adjust opacity to make it darker */
       pointer-events: none;
     }
