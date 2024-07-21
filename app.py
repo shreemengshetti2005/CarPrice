36c5f4cd094c909492ca54394e7b41ab5ec6167b
Soham7997 <161009997+Soham7997@users.noreply.github.com>
2024-07-21 10:59:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -145,11 +145,11 @@ if st.button("Predict Price"):
         st.write(custom_df.head())
         # Process the custom_df as needed
 
-    st.subheader("Sample Predictions")
-    sample_data = df.sample(5)
-    sample_predictions = loaded_model.predict(xgb.DMatrix(scaler.transform(sample_data)))
-    sample_data['predicted_selling_price'] = scaler_y.inverse_transform(pd.DataFrame(sample_predictions))
-    st.write(sample_data[['brand', 'model', 'vehicle_age', 'km_driven', 'mileage', 'predicted_selling_price']])
+   # st.subheader("Sample Predictions")
+    #sample_data = df.sample(5)
+    #sample_predictions = loaded_model.predict(xgb.DMatrix(scaler.transform(sample_data)))
+    #sample_data['predicted_selling_price'] = scaler_y.inverse_transform(pd.DataFrame(sample_predictions))
+    #st.write(sample_data[['brand', 'model', 'vehicle_age', 'km_driven', 'mileage', 'predicted_selling_price']])
 
     st.sidebar.subheader("Need Help?")
     st.sidebar.info("If you have any questions or need assistance, please contact our support team.")
